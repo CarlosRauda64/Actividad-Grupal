9efc0aa24a12f34464ad22c53baa8ad017825239
MarcelaGomez <marcelasaraigomezcanas@gmail.com>
2024-08-25 17:10:16 -1200

    Logica de calculo de la nota

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let nota = 0;
+let grade = 0;
 
 const correctAnswer = ["A","C","D","B"]
 
@@ -10,9 +10,13 @@ function test(){
     for (let i= 0; i< correctAnswer.length; i++){
         let userAnswer = document.querySelector('#question'+(i+1)).value;
         if (userAnswer === correctAnswer[i]){
+            grade += 1;
             alert("La respuesta a la pregunta " + (i+1) +" es correcta.")
         }else{
             alert("La respuesta a la pregunta " + (i+1) +" es incorrecta.")
         }
     }
+    grade = (grade * (10/4));
+    alert("Su nota final es: " + grade)
 }
+
