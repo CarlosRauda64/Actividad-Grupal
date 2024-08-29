206649a7eff5dc85210007dfcacecbb62060880c
MarcelaGomez <marcelasaraigomezcanas@gmail.com>
2024-08-28 16:47:30 -1200

    Modficiación del DOM y muestra de la nota eliminación de alerts

--- main.js
+++ main.js
@@ -1,22 +1,46 @@
 let grade = 0;
 
-const correctAnswer = ["A","C","D","B"]
+const correctAnswer = ["A","C","D","B"] //Declaración de respuestas correctas según ek número de preguntas preestablecidas
 
-const button = document.querySelector('button');
+const button = document.querySelector('button'); //lamamos el boton
 
 button.addEventListener('click',test)
 
 function test(){
-    for (let i= 0; i< correctAnswer.length; i++){
-        let userAnswer = document.querySelector('#question'+(i+1)).value;
+
+    let disableAnswers = document.querySelectorAll('select');
+    disableAnswers.forEach(function(select) {
+    select.disabled = true;
+    });
+
+    //Se crea un for que itera sobre el arreglo correctAnswer para ir evaluando cada respuesta
+    for (let i= 0; i< correctAnswer.length; i++){ 
+        let userAnswer = document.querySelector('#question'+(i+1)).value; //obtiene la respuesta del usuario
+        let feedback =  document.querySelector('.statusanswer' + (i + 1));
+        //Evaluación de la respuesta
         if (userAnswer === correctAnswer[i]){
             grade += 1;
-            alert("La respuesta a la pregunta " + (i+1) +" es correcta.")
+            feedback.textContent = "La respuesta a la pregunta " + (i + 1) + " es correcta.";
         }else{
-            alert("La respuesta a la pregunta " + (i+1) +" es incorrecta.")
+            feedback.textContent = "La respuesta a la pregunta " + (i + 1) + " es incorrecta.";
         }
     }
-    grade = (grade * (10/4));
-    alert("Su nota final es: " + grade)
+    
+    // calculo de nota final
+    grade = (grade * (10/4)); 
+
+    //Se crea un espacio de visualización para ver la nota 
+
+    const headerContainer = document.createElement('header');
+    headerContainer.className = 'header-container';
+
+    const title = document.createElement('h1');
+    title.className = 'title';
+    title.textContent = 'Nota final: ' + grade;
+
+    headerContainer.appendChild(title);
+    document.body.appendChild(headerContainer);
+
+
 }
 
